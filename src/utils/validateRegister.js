e63c3b12a23f4dc6df7d624201dee7c7048fae4a
Dmitry Shevtsov <dshevtsov20@gmail.com>
2024-05-14 16:51:47 +0300

    fix validation message

--- src/utils/validateRegister.js
+++ src/utils/validateRegister.js
@@ -2,7 +2,7 @@ const validateRegister = (values) => {
   const errors = {};
 
   if (!values.email) {
-    errors.email = "Required";
+    errors.email = "Неверный e-mail";
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
     errors.email = "Неверный e-mail";
   }
